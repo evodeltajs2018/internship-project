9a0b42aabb7ead4b178f1cac9b7344d17504ff2e
Sergiu Bucur <sergiu.bucur@evozon.com>
2018-07-30 17:09:02 +0300

    add backend part (not working yet)

--- backend/src/main.js
+++ backend/src/main.js
@@ -4,16 +4,6 @@ const app = express();
 
 app.use(cors());
 
-app.get("/dashboard", (req, res) => {
-	res.json({
-		test: "abc",
-		items: [
-			1,
-			2,
-			3,
-			4
-		]
-	});
-});
+require("./routes/DashboardRoute")(app);
 
 app.listen(5000, () => console.log("Listening on port 5000..."));
---
+++ backend/src/routes/DashboardRoute.js
@@ -0,0 +1,30 @@
+const sql = require("mssql");
+
+function dashboardRoute(app) {
+	app.get("/dashboard", (req, res) => {
+		// const config = {
+        //     user: "internship_user",
+        //     password: "internship_user",
+        //     server: "localhost",
+        //     database: "InternshipProject",
+		// 	port: 1333
+        // };
+
+		// sql.connect(config, err => {
+		// 	new sql.Request().query("select * from users", (err, result) => {
+		// 		if (err) {
+		// 			console.log("sql error", err);
+		// 			return;
+		// 		}
+
+		// 		res.json(result);
+		// 	});
+		// });
+
+		res.json({
+			test: "abc"
+		});
+	});
+}
+
+module.exports = dashboardRoute;
