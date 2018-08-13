923f9650ec865377f02dab4e30e8556d9a60b30b
bledeabogdan <bogdan.bledea@evozon.com>
2018-08-13 15:44:24 +0300

    database refactor and pagination css refactoring

--- backend/db_scripts/SoundsSeed.sql
+++ backend/db_scripts/SoundsSeed.sql
@@ -1,22 +1,26 @@
 USE [InternshipProject]
 GO
 
-INSERT INTO [dbo].[SoundType]
-           ([Name])
+INSERT INTO [dbo].[Type]
+           ([Name]
+           ,[IconSrc]
+           ,[ColorType])
      VALUES
-           ('Clap'),
-		   ('Beat'),
-		   ('Snare'),
-		   ('Transition'),
-		   ('Electronic'),
-		   ('Piano'),
-		   ('Guitar'),
-		   ('Voice')
+           ('Kick Drum','/src/img/sound-types/kick_drum.png','#0dc75b'),
+           ('Snare','/src/img/sound-types/snare.png','#ff054a'),
+           ('Closed Hihat','/src/img/sound-types/closed_hihat.png','#ff7317'),
+           ('Open Hihat','/src/img/sound-types/open_hihat.png','#1acbf7'),
+           ('Percussion','/src/img/sound-types/percussion.png','#8056f5'),
+           ('Fx','/src/img/sound-types/fx.png','#ff74b6'),
+           ('Synth','/src/img/sound-types/synth.png','#c6194d'),
+           ('Voice','/src/img/sound-types/voice.png','#1d53ff'),
+           ('Guitar','/src/img/sound-types/guitar.png','#19b7c6'),
+           ('Piano','/src/img/sound-types/piano.png','#6fc619')
 GO
 
 INSERT INTO [dbo].[Sound]
            ([Name]
-           ,[SoundTypeId])
+           ,[TypeId])
      VALUES
            ('Sound 1', 1),
            ('Sound 2', 3),
