d631eb824474904cc7d3b921e3622a8650c118cb
tividan2014 <tividan@gmail.com>
2018-08-13 10:25:10 +0300

    Refactor: Split Node controllers from routes

---
+++ backend/src/controllers/SoundController.js
@@ -0,0 +1,64 @@
+const SoundService = require("../services/SoundService");
+
+class SoundController {
+    constructor() {}
+
+    getAll(req, res) {
+        const page = req.query.page;
+        const itemsPerPage = req.query.perpage;
+        if (page && itemsPerPage) {
+            return SoundService.getAll(page, itemsPerPage, {
+                    name: req.query.name,
+                    type: req.query.type
+                })
+                .then((data) => {
+                    return res.json(data);
+                });
+
+        } else {
+            return res.json({});
+        }
+
+    }
+
+    addSound(req, res) {
+        let sound = req.body;
+        return SoundService.addSound(sound).then((result) => {
+            return res.json(result);
+        })
+    }
+
+    deleteSound(req, res) {
+        let id = req.params.id;
+        return SoundService.delete(id).then((result) => {
+            if (result) {
+                return res.json(result);
+            } else {
+                return res.json({});
+            }
+        });
+    }
+
+    editSound(req, res) {
+        let sound = req.body,
+            id = req.params.id;
+        return SoundService.editSound(sound, id).then((result) => {
+            return res.json(result);
+        });
+    }
+
+    getTypes(req, res) {
+        return SoundService.getTypes().then((result) => {
+            return res.json(result);
+        });
+    }
+
+    getSoundById(req, res) {
+        let id = req.params.id;
+        return SoundService.getSoundById(id).then((result) => {
+            return res.json(result);
+        });
+    }
+}
+
+module.exports = new SoundController();
---
+++ backend/src/routes/ProjectRoute.js
@@ -0,0 +1,33 @@
+const ProjectController = require("../controllers/ProjectController");
+
+class ProjectRoute {
+    constructor(app) {
+        this.app = app;
+
+        this.initRoutes();
+    }
+
+    initRoutes() {
+        this.app.get("/projects", (req, res) => {
+            ProjectController.getAllProjects(res);
+        });
+
+        this.app.post("/projects", (req, res) => {
+            ProjectController.addProject(req, res);
+        });
+
+        this.app.get("/projects/:id", (req, res) => {
+            ProjectController.getProjectById(req, res);
+        });
+
+        this.app.put("/projects/:id", (req, res) => {
+            ProjectController.editProject(req, res);
+        });
+
+        this.app.delete("/projects/:id", (req, res) => {
+            ProjectController.deleteProject(req, res);
+        });
+    }
+}
+
+module.exports = ProjectRoute;
