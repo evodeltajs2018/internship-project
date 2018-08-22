ae5b00318c058bdc55b2b95235a48b3ba4a76941
bledeabogdan <bogdan.bledea@evozon.com>
2018-08-22 14:57:16 +0300

    login authentication route, service created

--- backend/src/services/AuthenticationService.js
+++ backend/src/services/AuthenticationService.js
@@ -6,15 +6,36 @@ class AuthenticationService {
 
     }
 
-    verifyEmail(email) {
+    verifyIfEmailExists(email) {
         return DbConnection.executePoolRequest()
             .then(pool => {
                 return pool.input('email', DbConnection.sql.NVarChar(50), email)
-                    .query("SELECT * FROM Users WHERE Users.email = @email")
+                    .query("SELECT * FROM Users WHERE Users.Email = @email")
             })
             .then(result => {
                 return !(result.rowsAffected[0] === 1);
             });
+    }
+
+    checkEmail(email) {
+        return DbConnection.executePoolRequest()
+            .then(pool => {
+                return pool.input('email', DbConnection.sql.NVarChar(50), email)
+                    .query("SELECT * FROM Users WHERE Users.Email = @email")
+            })
+            .then(result => {
+                return result.rowsAffected[0] === 1;
+            });
+    }
+
+    getHashedPasswordByEmail(email){
+        return DbConnection.executePoolRequest()
+        .then(pool => {
+            return pool.input('email', DbConnection.sql.NVarChar(50), email)
+            .query("SELECT Id, Password From Users WHERE Users.Email = @email")
+        })
+        .then(result => result.recordset[0].Password && result.recordset[0].Id ? { id: result.recordset[0].Id, hash: result.recordset[0].Password} : null);
+
     }
 
     register(data) {
@@ -27,7 +48,7 @@ class AuthenticationService {
                     .input('email', DbConnection.sql.NVarChar(50), data.email)
                     .input('hashedPassword', DbConnection.sql.NVarChar(DbConnection.sql.MAX), data.hashedPassword)
                     .query(`INSERT INTO Users VALUES (@firstName, @lastName, @username, @email, @hashedPassword, 2);
-                            SELECT Id FROM Users WHERE Email = @email`);
+                            SELECT Id FROM Users WHERE Users.Email = @email`);
             })
             .then(result => result.recordset[0].Id ? result.recordset[0].Id : null);
     }
